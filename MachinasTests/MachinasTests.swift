52d68e6fb50ff2eed01f0020fbb0d0cc3fa32412
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2022-12-30 12:47:04 +0700

    Chore: Add unit test

---
+++ MachinasTests/MachinasTests.swift
@@ -0,0 +1,24 @@
+//
+//  MachinasTests.swift
+//  MachinasTests
+//
+//  Created by Timotius Leonardo Lianoto on 30/12/22.
+//
+
+import XCTest
+
+final class MachinasTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+
+}
